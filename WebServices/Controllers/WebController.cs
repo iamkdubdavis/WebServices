6e23721e1fa5e9440539e80c7cef050f74782a43
Kimberly Davis <kwatsondavis@gmail.com>
2015-09-29 10:37:49 -0400

    Initial Commit

---
+++ WebServices/Controllers/WebController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebServices.Models;
+
+namespace WebServices.Controllers
+{
+    public class WebController : ApiController
+    {
+        private ReservationRepository repo = ReservationRepository.Current;
+
+        public IEnumerable<Reservation> GetAllReservations()
+        {
+            return repo.GetAll();
+        }
+
+        public Reservation GetReservation(int id)
+        {
+            return repo.Get(id);
+        }
+
+        [System.Web.Http.HttpPost]
+        public Reservation CreateReservation(Reservation item)
+        {
+            return repo.Add(item);
+        }
+
+        [System.Web.Http.HttpPut]
+        public bool UpdateReservation(Reservation item)
+        {
+            return repo.Update(item);
+        }
+
+        public void DeleteReservation(int id)
+        {
+            repo.Remove(id);
+        }
+    }
+}
